7d8020a2785970ba599fb092776063d3931a70a8
Joseph Dang <josephddang00@gmail.com>
2022-12-19 16:53:57 -0500

    revisons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import Analytics from './componets/Analytics';
+import Cards from './componets/Cards';
+import Footer from './componets/Footer';
+import Hero from './componets/Hero';
 import Navbar from './componets/Navbar';
-import Hero from "./componets/Hero";
-import Analytics from "./componets/Analytics";
-import Newsletter from "./componets/Newsletter";
-import Cards from "./componets/Cards";
-import Footer from "./componets/Footer";
+import Newsletter from './componets/Newsletter';
+
 
 
 function App() {
